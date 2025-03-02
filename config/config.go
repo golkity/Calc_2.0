c4e914cd11c46707b9b11f6600a7ff44cc464a73
Finnik <73883383+golkity@users.noreply.github.com>
2025-03-02 10:08:32 +0300

    Update: add link to custom errors and new logic decod json

--- config/config.go
+++ config/config.go
@@ -2,27 +2,26 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"lms/internal/custom_errors"
 	"os"
 )
 
 type Config struct {
-	ServerPort string `json:"server_port"`
-	LogFile    string `json:"log_file"`
+	ServerPort string `json:"Port"`
+	LogFile    string `json:"Log"`
 }
 
 func LoadConfig(configPath string) (*Config, error) {
 	file, err := os.Open(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("unable to open config file: %v", err)
+		return nil, custom_errors.ErrOpenConfiguration
 	}
 	defer file.Close()
 
 	var config Config
-	byteValue, _ := ioutil.ReadAll(file)
-	if err := json.Unmarshal(byteValue, &config); err != nil {
-		return nil, fmt.Errorf("unable to parse config file: %v", err)
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, custom_errors.ErrParceConfiguration
 	}
 
 	return &config, nil
