a4af599f7804c19f19aa3127c00970a3272ef8dd
Finnik <73883383+golkity@users.noreply.github.com>
2025-03-02 09:57:54 +0300

    Update: add full logic in file

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,22 @@
-package agent
+package main
+
+import (
+	"log"
+	"os"
+
+	"lms/internal/applicant"
+)
+
+func main() {
+	configPath := "./config/config.json"
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
+
+	agentApp, err := applicant.AgentNew(configPath)
+	if err != nil {
+		log.Fatalf("Failed to init agent: %v", err)
+	}
+
+	agentApp.Run()
+}
