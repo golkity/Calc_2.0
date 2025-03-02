dfc5065153031bac40c8c5eca6f4005c7f5535ff
Finnik <73883383+golkity@users.noreply.github.com>
2025-03-02 09:58:15 +0300

    Update: add full logic in file

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,5 +1,22 @@
-package orchestrator
+package main
+
+import (
+	"log"
+	"os"
+
+	"lms/internal/applicant"
+)
 
 func main() {
+	configPath := "./config/config.json"
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
 
+	orchApp, err := applicant.OrchestratorNew(configPath)
+	if err != nil {
+		log.Fatalf("Failed to init orchestrator: %v", err)
+	}
+
+	orchApp.Run()
 }
