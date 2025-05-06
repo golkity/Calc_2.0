1c43b390ef6a5fe7f5eabf9fd27052672947e5f2
Finnik <73883383+golkity@users.noreply.github.com>
2025-05-06 08:06:43 +0300

    Udpate: new struct in service

---
+++ service/api-gateway/internal/kafka/producer.go
@@ -0,0 +1,40 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
+)
+
+const topicTasks = "calc.tasks"
+
+type Producer struct{ w *kafka.Writer }
+
+type Pinger interface {
+	Ping(ctx context.Context) error
+}
+
+func New(brokers []string) *Producer {
+	return &Producer{w: &kafka.Writer{
+		Addr:     kafka.TCP(brokers...),
+		Topic:    topicTasks,
+		Balancer: &kafka.Hash{},
+	}}
+}
+
+func (p *Producer) PublishCalcRequest(ctx context.Context, userID int64, expr string) error {
+	msg := map[string]any{"user_id": userID, "expression": expr}
+	b, _ := json.Marshal(msg)
+	return p.w.WriteMessages(ctx, kafka.Message{
+		Key:   []byte(strconv.FormatInt(userID, 10)),
+		Value: b,
+	})
+}
+
+func (p *Producer) Ping(ctx context.Context) error {
+	return p.w.WriteMessages(ctx, kafka.Message{Value: []byte(`{}`)})
+}
+
+func (p *Producer) Close() error { return p.w.Close() }
