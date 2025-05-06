38820b68972f39e5d2354bfe8bff68e2d04df20a
Finnik <73883383+golkity@users.noreply.github.com>
2025-05-06 08:05:32 +0300

    Update: new struct for service

--- service/api-gateway/cmd/main.go
+++ service/api-gateway/cmd/main.go
@@ -2,36 +2,14 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
-	"strings"
 
-	"api-gateway/handler"
-	"api-gateway/kafka"
-	"api-gateway/middleware"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/golkity/Calc_2.0/pkg/tokens"
+	"api-gateway/internal/server/http"
 )
 
 func main() {
-	brokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
-	prod := kafka.New(brokers)
-	// exprRepo := repository.NewExpr(dbConn)
-	defer prod.Close()
-
-	tm := tokens.NewManager([]byte(os.Getenv("JWT_SECRET")), 0, 0)
-
-	r := chi.NewRouter()
-	r.Get("/healthz", handler.Healthz(prod))
-	r.Group(func(rt chi.Router) {
-		rt.Use(middleware.Auth(tm))
-		rt.Post("/api/v1/calculate", handler.Calculate(prod))
-		// rt.Get("/expressions/{id}", handler.GetExpression(exprRepo))
-	})
-
-	log.Println("api-gateway :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	srv, err := http.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(srv.Start())
 }
-
-//TODO: add expression get by ID!
