9f1d425b017bdde0cf555fdf2634fd22665245a0
Finnik <73883383+golkity@users.noreply.github.com>
2025-05-09 12:14:22 +0300

    create routes.go to centralize API endpoint registration (/healthz, /api/v1)

--- service/auth/internal/http/routes/routes.go
+++ service/auth/internal/http/routes/routes.go
@@ -1,16 +1,15 @@
-// package http
-
-// import (
-// 	"github.com/go-chi/chi/v5"
-// )
-
-//	func (h *Handler) RegisterRoutes(r chi.Router) {
-//		r.Post("/register", h.Register)
-//		r.Post("/login", h.Login)
-//		r.Post("/refresh", h.Refresh)
-//		r.Route("/api", func(r chi.Router) {
-//			r.Use(h.Auth)
-//			r.Get("/me", h.Me)
-//		})
-//	}
 package routes
+
+import (
+	h "auth_service/internal/http/handler"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Register(r chi.Router, hnd *h.Handler) {
+	r.Route("/api/v1", func(rt chi.Router) {
+		rt.Post("/register", hnd.Register)
+		rt.Post("/login", hnd.Login)
+		rt.Post("/token/refresh", hnd.Refresh)
+	})
+}
