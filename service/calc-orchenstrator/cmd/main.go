22334ae309e3f109102416d5e7af35104daf9b6b
Finnik <73883383+golkity@users.noreply.github.com>
2025-05-09 12:17:26 +0300

    Update: extract HTTP server & routes, keep main as minimal bootstrap

--- service/calc-orchenstrator/cmd/main.go
+++ service/calc-orchenstrator/cmd/main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"os"
-	"strings"
 
-	kaf "github.com/golkity/Calc_2.0/service/calc-orchenstrator/kafka"
-	orchestrator "github.com/golkity/Calc_2.0/service/calc-orchenstrator/orchenstrator"
-	"github.com/golkity/Calc_2.0/service/calc-orchenstrator/repository"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5"
+	"github.com/golkity/Calc_2.0/service/calc-orchenstrator/internal/http/server"
 )
 
 func main() {
-	ctx := context.Background()
-
-	db, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
+	srv, err := server.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close(ctx)
-
-	exprRepo := repository.NewExpr(db)
-	prod := kaf.New(strings.Split(os.Getenv("KAFKA_BROKERS"), ","))
-
-	svc := orchestrator.New(exprRepo, prod)
-
-	go func() {
-		_ = kaf.Consume(ctx, strings.Split(os.Getenv("KAFKA_BROKERS"), ","), func(t kaf.TaskReq) {
-			svc.Handle(ctx, t.UserID, t.Expression)
-		})
-	}()
-
-	r := chi.NewRouter()
-	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
-		if err := db.Ping(ctx); err != nil {
-			http.Error(w, "db down", 503)
-			return
-		}
-		w.WriteHeader(200)
-	})
-	log.Println("orchestrator :8090")
-	log.Fatal(http.ListenAndServe(":8090", r))
+	log.Fatal(srv.Start())
 }
