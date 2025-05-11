2d6793b8a8d98c3355d9b851f76513c23c5ead30
Finnik <73883383+golkity@users.noreply.github.com>
2025-05-11 22:32:37 +0300

    Update: add inteface for test

--- service/api-gateway/internal/handler/list_exp.go
+++ service/api-gateway/internal/handler/list_exp.go
@@ -2,21 +2,27 @@ package handler
 
 import (
 	"api-gateway/internal/middleware"
+	"context"
 	"encoding/json"
 	"net/http"
 
 	"github.com/golkity/Calc_2.0/service/calc-orchenstrator/repository"
 )
 
-func ListExpressions(repo *repository.ExpressionRepo) http.HandlerFunc {
+type ExpressionLister interface {
+	List(ctx context.Context, userID int64) ([]repository.Expression, error)
+}
+
+func ListExpressions(repo ExpressionLister) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		uid := middleware.UserID(r.Context())
 		list, err := repo.List(r.Context(), uid)
 		if err != nil {
-			http.Error(w, err.Error(), 500)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		w.Header().Set("Content-Type", "application/json")
 		_ = json.NewEncoder(w).Encode(struct {
 			Expressions []repository.Expression `json:"expressions"`
 		}{list})
